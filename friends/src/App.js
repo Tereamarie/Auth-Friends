25c04a6f779bae0f7643ca995580ff7d984da8cf
marie-hotchkiss <mtknowlton67@gmail.com>
2020-06-09 08:57:04 -0700

    added component/utilities folder/modified index.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link, Router, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="nav">
+       <Link to="/login">Login</Link>
+       <Link to="/protected">Friends</Link>
+       <Link to="/addFriend">Add a Friend</Link>
+      </nav>
+      <Switch>
+      
+      </Switch>
     </div>
   );
 }
